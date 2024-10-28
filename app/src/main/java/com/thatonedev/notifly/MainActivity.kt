58da6996c04b72aaaf21b441ff6c9f414ca869bb
JulienToumieh <julientoumieh2@gmail.com>
2024-10-28 15:21:31 +0200

    Added component and recyclerview for rules + improvements

--- app/src/main/java/com/thatonedev/notifly/MainActivity.kt
+++ app/src/main/java/com/thatonedev/notifly/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.thatonedev.notifly
 
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.PowerManager
 import android.provider.Settings
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.thatonedev.notifly.activities.PermissionActivity
+import com.thatonedev.notifly.components.RuleComponent
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +31,42 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        startActivity(Intent(Intent(this, PermissionActivity::class.java)))
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        val isIgnoringBatteryOptimizations = powerManager.isIgnoringBatteryOptimizations(packageName)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val isNotificationListenerEnabled = notificationManager.isNotificationPolicyAccessGranted
 
+        if (!isIgnoringBatteryOptimizations or !isNotificationListenerEnabled) {
+            startActivity(Intent(Intent(this, PermissionActivity::class.java)))
+        }
+
+
+        val task1 = JSONObject().apply {
+            put("name", "Click on the checkbox to complete task.")
+            put("active", false)
+        }
+
+        val task2 = JSONObject().apply {
+            put("name", "Click and hold on a task to delete.")
+            put("active", true)
+        }
+        val task3 = JSONObject().apply {
+            put("name", "Click on the 'Retaskd' text at the top to open the backup/restore popup.")
+            put("active", false)
+        }
+
+
+        val ruleArray = JSONArray().apply {
+            put(task1)
+            put(task2)
+            put(task3)
+        }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rule_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = RuleComponent(this, ruleArray)
+        recyclerView.adapter = adapter
     }
+
 
 }
---
+++ app/src/main/java/com/thatonedev/notifly/components/RuleComponent.kt
@@ -0,0 +1,41 @@
+package com.thatonedev.notifly.components
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Switch
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.thatonedev.notifly.R
+import org.json.JSONArray
+
+class RuleComponent(private val activity: Activity, private val dataSet: JSONArray) : RecyclerView.Adapter<RuleComponent.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val ruleName: TextView = view.findViewById(R.id.rule_name)
+        val ruleSwitch: Switch = view.findViewById(R.id.rule_switch)
+        val ruleCard: CardView = view.findViewById(R.id.rule_card)
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.component_rule, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.ruleName.text = dataSet.getJSONObject(position).getString("name").toString()
+        viewHolder.ruleSwitch.isChecked = dataSet.getJSONObject(position).getBoolean("active")
+
+        viewHolder.ruleCard.setOnClickListener {
+
+        }
+    }
+
+    override fun getItemCount() = dataSet.length()
+
+    interface OnDataPass {
+        fun toggleTask(position: Int)
+    }
+}
